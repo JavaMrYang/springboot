68d9d62c64d3b06135003bbc19c314e0e5004756
liuyang <liuyang@tcloudata.com>
2020-08-04 19:27:17 +0800

    update code

--- springboot-01/src/main/java/com/study/web/CustomErrorWebExceptionHandler.java
+++ springboot-01/src/main/java/com/study/web/CustomErrorWebExceptionHandler.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWe
 import org.springframework.boot.web.reactive.error.ErrorAttributes;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 /**
@@ -14,11 +13,16 @@ import org.springframework.web.reactive.function.server.ServerResponse;
  * @Date 2019/5/31/031  14:40
  * @Version 1.0
  **/
+/*
+
+
+ */
 
 /**
  * 要更改错误处理行为，可以实现ErrorWebExceptionHandler并注册该类型的bean定义。因为WebExceptionHandler是非常底层的，
  * Spring Boot还提供了一个方便的AbstractErrorWebExceptionHandler，让您可以用WebFlux函数的方式处理错误
  */
+
 public class CustomErrorWebExceptionHandler extends AbstractErrorWebExceptionHandler {
 
 
